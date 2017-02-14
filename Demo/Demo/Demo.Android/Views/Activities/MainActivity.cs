d0bd3a18bfa2594feb5039d84a48352e046a76bd
Luke Pothier <lukepothier@gmail.com>
2017-02-14 20:24:16 +0200

    Added app

---
+++ Demo/Demo/Demo.Android/Views/Activities/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using MvvmCross.Droid.Platform;
+using Demo.Core.ViewModels;
+
+namespace Demo.Android.Views
+{
+    [Activity(
+        Theme = "@style/Demo.Android",
+        WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden)]
+    public class MainActivity : BaseActivity<MainContainerViewModel>
+    {
+        protected override int ActivityLayoutId => Resource.Layout.layout_activity;
+    }
+}
---
+++ Demo/Demo/Demo.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.IoC;
+using Demo.Core.ViewModels;
+
+namespace Demo.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<FirstViewModel>();
+        }
+    }
+}
