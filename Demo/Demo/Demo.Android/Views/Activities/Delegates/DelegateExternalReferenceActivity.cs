d960c121e17d2e6c7e11d7f1247dd1793e16977b
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-02-18 15:11:33 +0200

    Android fix line endings and added delegate views to folder

--- Demo/Demo/Demo.Android/Views/Activities/Delegates/DelegateExternalReferenceActivity.cs
+++ Demo/Demo/Demo.Android/Views/Activities/Delegates/DelegateExternalReferenceActivity.cs
@@ -9,20 +9,21 @@ namespace Demo.Android.Views
     [Activity(
        Theme = "@style/Demo.Android",
        WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden)]
-    public class DelegateLocalReferenceActivity : BaseActivity<DelegateLocalReferenceViewModel>
+    public class DelegateExternalReferenceActivity : BaseActivity<DelegateExternalReferenceViewModel>
     {
         protected override int ActivityLayoutId => Resource.Layout.layout_activity_generic_delegate;
-        protected override int TitleResourceId => Resource.String.toolbar_localref_function;
+        protected override int TitleResourceId => Resource.String.toolbar_externalref_function;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             var button = FindViewById<Button>(Resource.Id.button_close);
+            button.Text = Resources.GetString(Resource.String.button_message);
+
             button.Click += delegate
             {
-                System.Diagnostics.Debug.WriteLine($"Closing {nameof(DelegateLocalReferenceActivity)}");
-                this.Finish();
+                System.Diagnostics.Debug.WriteLine($"Clicked {nameof(DelegateExternalReferenceActivity)}");
             };
         }
     }
