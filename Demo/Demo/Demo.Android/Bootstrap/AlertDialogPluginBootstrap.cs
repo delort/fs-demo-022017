f1b84db63dc33162d18080c0b4585644f438278c
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-02-19 17:01:01 +0200

    Added demo alert dialog nuget package

---
+++ Demo/Demo/Demo.Android/Bootstrap/AlertDialogPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Demo.Android.Bootstrap
+{
+    public class AlertDialogPluginBootstrap
+        : MvxPluginBootstrapAction<Demo.Plugin.AlertDialog.PluginLoader>
+    {
+    }
+}
---
+++ Demo/Demo/Demo.iOS/Bootstrap/AlertDialogPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Demo.iOS.Bootstrap
+{
+    public class AlertDialogPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Demo.Plugin.AlertDialog.PluginLoader, Demo.Plugin.AlertDialog.iOS.Plugin>
+    {
+    }
+}
