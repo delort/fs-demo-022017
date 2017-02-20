039fb022c339262b432b9161eb8715f805152c84
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-02-20 23:13:24 +0200

    Add command creation method

--- Demo/Demo/Demo.Core/ViewModels/Base/BaseViewModel.cs
+++ Demo/Demo/Demo.Core/ViewModels/Base/BaseViewModel.cs
@@ -1,21 +1,29 @@
-﻿using MvvmCross.Core.ViewModels;
+﻿using System;
+using MvvmCross.Core.ViewModels;
 
 namespace Demo.Core.ViewModels
 {
-    public abstract class BaseViewModel: MvxViewModel
-    {
-        #region Commands
+	public abstract class BaseViewModel : MvxViewModel
+	{
+		#region Commands
 
-        IMvxCommand _navigateBackCommand;
-        public IMvxCommand NavigateBackCommand =>
-            _navigateBackCommand ?? (_navigateBackCommand = new MvxCommand(GoBack));
+		IMvxCommand _navigateBackCommand;
+		public IMvxCommand NavigateBackCommand
+			=> CreateCommand(ref _navigateBackCommand, GoBack);
 
-        #endregion
+		#endregion
 
-        #region Navigation
+		#region Navigation
 
-        void GoBack() => Close(this);
+		void GoBack() => Close(this);
 
-        #endregion
-    }
+		#endregion
+
+		#region Helpers
+
+		public IMvxCommand CreateCommand(ref IMvxCommand localCommand, Action commandExecution, Func<bool> canExecuteCommand = null)
+			=> localCommand ?? (localCommand = new MvxCommand(commandExecution, canExecuteCommand));
+
+		#endregion
+	}
 }
