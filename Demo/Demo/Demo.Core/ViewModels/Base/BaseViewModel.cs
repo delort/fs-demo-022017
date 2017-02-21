77c378f469eae28d292ce3101d517afb7001ddf4
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-02-21 22:23:12 +0200

    Fix tabing of XS

--- Demo/Demo/Demo.Core/ViewModels/Base/BaseViewModel.cs
+++ Demo/Demo/Demo.Core/ViewModels/Base/BaseViewModel.cs
@@ -3,26 +3,26 @@ using MvvmCross.Core.ViewModels;
 
 namespace Demo.Core.ViewModels
 {
-	public abstract class BaseViewModel : MvxViewModel
-	{
-		#region Commands
+    public abstract class BaseViewModel : MvxViewModel
+    {
+        #region Commands
 
-		IMvxCommand _navigateBackCommand;
-		public IMvxCommand NavigateBackCommand => CreateCommand(ref _navigateBackCommand, GoBack);
+        IMvxCommand _navigateBackCommand;
+        public IMvxCommand NavigateBackCommand => CreateCommand(ref _navigateBackCommand, GoBack);
 
-		#endregion
+        #endregion
 
-		#region Navigation
+        #region Navigation
 
-		void GoBack() => Close(this);
+        void GoBack() => Close(this);
 
-		#endregion
+        #endregion
 
-		#region Helpers
+        #region Helpers
 
-		public IMvxCommand CreateCommand(ref IMvxCommand localCommand, Action commandExecution, Func<bool> canExecuteCommand = null)
-			=> localCommand ?? (localCommand = new MvxCommand(commandExecution, canExecuteCommand));
+        public IMvxCommand CreateCommand(ref IMvxCommand localCommand, Action commandExecution, Func<bool> canExecuteCommand = null)
+            => localCommand ?? (localCommand = new MvxCommand(commandExecution, canExecuteCommand));
 
-		#endregion
-	}
+        #endregion
+    }
 }
--- Demo/Demo/Demo.Core/ViewModels/Delegate Functions/DelegateFunctionsViewModel.cs
+++ Demo/Demo/Demo.Core/ViewModels/Delegate Functions/DelegateFunctionsViewModel.cs
@@ -2,34 +2,34 @@
 
 namespace Demo.Core.ViewModels
 {
-	public class DelegateFunctionsViewModel : BaseViewModel
-	{
-		#region Commands
+    public class DelegateFunctionsViewModel : BaseViewModel
+    {
+        #region Commands
 
-		IMvxCommand _exportFunctionCommand;
-		public IMvxCommand ExportFunctionCommand => CreateCommand(ref _exportFunctionCommand, GoToExportFunction);
+        IMvxCommand _exportFunctionCommand;
+        public IMvxCommand ExportFunctionCommand => CreateCommand(ref _exportFunctionCommand, GoToExportFunction);
 
-		IMvxCommand _localRefFunctionCommand;
-		public IMvxCommand LocalRefFunctionCommand => CreateCommand(ref _localRefFunctionCommand, GoToLocalRefFunction);
+        IMvxCommand _localRefFunctionCommand;
+        public IMvxCommand LocalRefFunctionCommand => CreateCommand(ref _localRefFunctionCommand, GoToLocalRefFunction);
 
-		IMvxCommand _externalRefFunctionCommand;
-		public IMvxCommand ExternalRefFunctionCommand => CreateCommand(ref _externalRefFunctionCommand, GoToExternalRefFunction);
+        IMvxCommand _externalRefFunctionCommand;
+        public IMvxCommand ExternalRefFunctionCommand => CreateCommand(ref _externalRefFunctionCommand, GoToExternalRefFunction);
 
-		IMvxCommand _subscriptionFunctionCommand;
-		public IMvxCommand SubscriptionFunctionCommand => CreateCommand(ref _subscriptionFunctionCommand, GoToSubscriptionFunctions);
+        IMvxCommand _subscriptionFunctionCommand;
+        public IMvxCommand SubscriptionFunctionCommand => CreateCommand(ref _subscriptionFunctionCommand, GoToSubscriptionFunctions);
 
-		#endregion
+        #endregion
 
-		#region Navigation
+        #region Navigation
 
-		void GoToExportFunction() => ShowViewModel<DelegateExportViewModel>();
+        void GoToExportFunction() => ShowViewModel<DelegateExportViewModel>();
 
-		void GoToLocalRefFunction() => ShowViewModel<DelegateLocalReferenceViewModel>();
+        void GoToLocalRefFunction() => ShowViewModel<DelegateLocalReferenceViewModel>();
 
-		void GoToExternalRefFunction() => ShowViewModel<DelegateExternalReferenceViewModel>();
+        void GoToExternalRefFunction() => ShowViewModel<DelegateExternalReferenceViewModel>();
 
-		void GoToSubscriptionFunctions() => ShowViewModel<DelegateSubscriptionViewModel>();
+        void GoToSubscriptionFunctions() => ShowViewModel<DelegateSubscriptionViewModel>();
 
-		#endregion
-	}
+        #endregion
+    }
 }
